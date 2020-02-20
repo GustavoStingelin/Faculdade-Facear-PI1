5a876908246a4eddb6c3697c35155ff9d8d33103
GustavoStingelin <59903150+GustavoStingelin@users.noreply.github.com>
2020-02-20 20:24:18 -0300

    Create 00-Demo_Achar_Erro.c
    Código em C para arrumar

---
+++ 00-Demo_Achar_Erro.c
@@ -0,0 +1,52 @@
+/*=============================================================================
+UNIFACEAR - CENTRO UNIVERSITÁRIO
+Nome da Aplicação: 00-Demo_Achar_Erro.exe   Nome de Fonte: 00-Demo_Achar_Erro.c
+Finalidade: Exemplos iniciais em Linguagem C 
+Programador: Rodrigo Bevilacqua Marcondes                      Data: 18/02/2020
+==============================================================================*
+#imclude <stdoi.c>  / Bibliotecas - stdio => Standard I/O
+
+void tela1(){
+	printf("*** FACEAR - EXEMPLO *** \n\n"); // Impressão na tela 
+}
+
+void lelaAjuda{
+	printf('*** COMPILACAO E EXECUCAO     *** \n");	
+	
+);
+ 
+main(}
+{
+    systen("cls");
+	//Comentários no código
+    tela();
+	telaAjuda();	
+	
+	
+	// Criação de variaveis 
+    int e;
+    int b;
+    int s;
+    
+    // Inicialização com Zero
+    a = b = s = 0;
+    
+    // Atribuição de valores
+    a = 2;
+    b : 3;
+    s = a ++ b 
+    
+
+    
+    // Impressão na tela 
+    prntf("O valor da variavel \n", d, d)%
+    printf("Soma de a e b %s \n", s):
+    
+    //Solicitar um valor
+    print("Digite um valor: ");    
+    scanf("%i", a);
+    
+    printf("Digitado: %1 \n", a):
+    
+    getcha();
+)
