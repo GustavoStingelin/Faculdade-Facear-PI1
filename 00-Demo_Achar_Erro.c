9adc741026322cc7f884d047588cc9c653962963
GustavoStingelin <59903150+GustavoStingelin@users.noreply.github.com>
2020-02-28 20:42:34 -0300

    Update 00-Demo_Achar_Erro.c
    Código corrigido.

--- 00-Demo_Achar_Erro.c
+++ 00-Demo_Achar_Erro.c
@@ -3,28 +3,26 @@ UNIFACEAR - CENTRO UNIVERSITÁRIO
 Nome da Aplicação: 00-Demo_Achar_Erro.exe   Nome de Fonte: 00-Demo_Achar_Erro.c
 Finalidade: Exemplos iniciais em Linguagem C 
 Programador: Rodrigo Bevilacqua Marcondes                      Data: 18/02/2020
-==============================================================================*
-#imclude <stdoi.c>  / Bibliotecas - stdio => Standard I/O
-
+==============================================================================*/
+#include <stdio.h>  // Bibliotecas - stdio => Standard I/O
 void tela1(){
 	printf("*** FACEAR - EXEMPLO *** \n\n"); // Impressão na tela 
 }
-
-void lelaAjuda{
-	printf('*** COMPILACAO E EXECUCAO     *** \n");	
+void leiaAjuda(){
+	printf("*** COMPILACAO E EXECUCAO     *** \n");	
 	
-);
+}
  
-main(}
+main()
 {
-    systen("cls");
+    system("cls");
 	//Comentários no código
-    tela();
-	telaAjuda();	
+    tela1();
+    leiaAjuda();	
 	
 	
 	// Criação de variaveis 
-    int e;
+    int a;
     int b;
     int s;
     
@@ -33,20 +31,19 @@ main(}
     
     // Atribuição de valores
     a = 2;
-    b : 3;
-    s = a ++ b 
+    b = 3;
+    s = a + b; 
     
-
     
     // Impressão na tela 
-    prntf("O valor da variavel \n", d, d)%
-    printf("Soma de a e b %s \n", s):
+    printf("O valor da variavel %d %d \n", a, b);
+    printf("Soma de a e b %d \n", s);
     
     //Solicitar um valor
-    print("Digite um valor: ");    
-    scanf("%i", a);
+    printf("Digite um valor: ");    
+    scanf("%i", &a);
     
-    printf("Digitado: %1 \n", a):
+    printf("Digitado: %i \n", a);
     
-    getcha();
-)
+    getchar();
+}
