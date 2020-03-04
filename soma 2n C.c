5f7d9a1dbc6259b88b21dd2e3b98c30a02717c3b
GustavoStingelin <59903150+GustavoStingelin@users.noreply.github.com>
2020-03-03 21:49:52 -0300

    Programas aprendizado 1

---
+++ soma 2n C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+main() {
+	
+	//var
+	float valor1;
+	float valor2;
+	float result;
+	
+	valor1 = valor2 = result = 0;	
+		
+	printf("******Programa para calcular a soma de 2 numeros****** \n\n\n");
+	
+	printf("Digite o primeiro valor: ");
+	scanf("%f", &valor1);
+	
+	printf("\nDigite o segundo valor: ");
+	scanf("%f", &valor2);
+	
+	result = valor1 + valor2;
+	printf("\nO resultado: %.2f", result);
+		
+	getchar;
+}
