9605abc9abbf8f2e3cccaf29541d7cf016970963
Gustavo Stingelin Cardoso <gustavo.stingelin.c@outlook.com>
2020-03-29 14:46:51 -0300

    Reorganizado

---
+++ zzOutros/calcular IMC C.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+main() {
+	
+	//var
+	float altura;
+	float peso;
+	float IMC;
+	float a;
+	altura = peso = IMC = 0;	
+		
+	printf("******Programa para calcular o IMC****** \n\n\n");
+	
+	printf("Digite a altura em Metros: ");
+	scanf("%f", &altura);
+	
+	printf("\nDigite o peso em Kilogramas: ");
+	scanf("%f", &peso);
+	
+	
+	
+	IMC = peso / (pow(altura, 2));
+		
+	printf("\nO resultado: %.2f", IMC);
+		
+	getchar;
+}
